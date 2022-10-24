133053b8cbc62ee6d71b982c1604da062f697e72
Andrey <andsul@mail.ru>
2022-10-24 18:51:23 +0300

    убрал заккоментированный код

--- src/main/java/com/sulima/controllers/ArticlesController.java
+++ src/main/java/com/sulima/controllers/ArticlesController.java
@@ -4,19 +4,15 @@ import com.sulima.data.ArticlesService;
 import com.sulima.data.CookieFactory;
 import com.sulima.data.MainService;
 import com.sulima.model.Articles;
-import com.sulima.model.Comments;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -55,29 +51,5 @@ public class ArticlesController {
         }
         return "/article";
     }
-
-//    @PostMapping("/list_articles")
-//    public List<Articles> listArticles() {
-//        return articlesService.getListArticles();
-//    }
-
-//    @GetMapping(path = "/article_one/{id}")
-//    public Articles articleOne(@PathVariable Integer id) {
-//        return articlesService.getArticleById(id);
-//    }
-
-//    @PostMapping("/comments_list")
-//    public List<Comments> commentsList(@RequestParam Integer id) {
-//        return articlesService.getListComments(id);
-//    }
-
-//    @PostMapping("/comment_add")
-//    public String commentAdd(@Validated Comments comment, BindingResult bindingResult) {
-//        String error = mainService.getError(bindingResult);
-//        if (error == "1") {
-//            articlesService.saveComment(comment);
-//        }
-//        return error;
-//    }
 }
 
